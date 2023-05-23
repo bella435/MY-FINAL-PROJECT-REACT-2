388fb9b2a1d9fbc02ad69ac88236ab89f189eb32
bella435 <mamadou.diallo19@lc.cuny.edu>
2023-05-23 17:49:32 -0400

    new change on the contact componnent

--- client/src/component/Contact.js
+++ client/src/component/Contact.js
@@ -1,15 +1,52 @@
 import React from "react";
-
+import { useState } from 'react';
 
 const Contact = () => {
-    return (
-        <div>
-            <h2>Contact</h2>
-            <h3>Bellagnaly@gmail.com</h3>
-            <h3>347-986-4581</h3>
 
-           
-        </div>
+
+    function MyForm() {
+        const [name, setName] = useState("");
+        const [email, setEmail] = useState("");
+        const handleSubmit = (event) => {
+          event.preventDefault();
+          alert(`The name you entered was: ${name}`)
+          console.log(email);
+
+        }
+      
+        return (
+          <form onSubmit={handleSubmit}>
+            <label>Name:
+              <input 
+                type="text" 
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+            <label>email:
+              <input 
+                type="email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <input type="submit" />
+          </form>
+        )
+      }
+
+
+
+
+    return (
+          
+              <MyForm/>
+    
     )
 }
 export default Contact
+
+
+{/* <h2>Contact</h2>
+            <h3>Bellagnaly@gmail.com</h3>
+            <h3>347-986-4581</h3> */}
