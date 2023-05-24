34148fcfaf1069b2261cc89984dd532bf1fddc1b
bella435 <mamadou.diallo19@lc.cuny.edu>
2023-05-23 23:04:32 -0400

    update queries file

--- server/queries.js
+++ server/queries.js
@@ -6,3 +6,15 @@ const pool = new Pool({
   password: 'billo',
   port: 5432,
 })
+
+const getContact = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+module.exports = {
+  getContact
+}
