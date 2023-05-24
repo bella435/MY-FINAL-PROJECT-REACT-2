6056ac186c2c454eeea13bf8cac484847683e532
bella435 <mamadou.diallo19@lc.cuny.edu>
2023-05-23 23:21:41 -0400

    postgres and update

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
+const bodyParser = require('body-parser');
 const path = require('path');
 const express = require('express')
+const db = require('./queries')
 
 const PORT = 8001
 
 const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 
@@ -17,3 +23,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, ()=>{
     console.log(`server is lintenning on port ${PORT}`)
 })
+
+
+app.get('/contact', db.getContact)
